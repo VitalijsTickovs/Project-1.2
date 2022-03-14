da7e11e4704ee381aa42964a4c7701563700a8ac
Niko Covic <niko.covic@gmail.com>
2022-03-14 12:20:01 +0100

    Added height map creator for terrain function

--- Phase 1/src/Data_storage/TerrainFunction1.java
+++ Phase 1/src/Data_storage/TerrainFunction1.java
@@ -12,6 +12,39 @@ public class TerrainFunction1 extends TerrainFunction {
         dfy = f.getDerivative("y");
     }
 
+    /**
+     * Gets the height map of the terrain function
+     * @param numVertecesX The number of verteces to include on the x-axis
+     * @param numVertecesY The number of verteces to include on the y-axis
+     * @param normalFactor The factor to scale the normalization by
+     * @return The height map as a 1D array
+     */
+    public float[] getHeightMap(int numVertecesX, int numVertecesY, double normalFactor) {
+        final double MINY = -10;
+        final double MAXY = 10;
+        float[] heightMap = new float[numVertecesX*numVertecesY];
+        // Loop through the points
+        for (int x=-numVertecesX/2; x<numVertecesX/2; x++) {
+            for (int y=-numVertecesY/2; y<numVertecesY/2; y++) {
+                double val = valueAt(x, y);
+                // Ensure min is 0
+                val += Math.abs(MINY);
+                // Normalize values
+                val /= MAXY-MINY;
+                // Clamp out of range
+                if (val > 1) {
+                    val = 1;
+                }
+                if (val < 0) {
+                    val = 0;
+                }
+                // Scale with normal factor
+                val *= normalFactor;
+            }
+        }
+        return heightMap;
+    }
+
     @Override
     public double valueAt(double x, double y) {
         return f.evaluate(new String[] {"x", "y"}, new double[] {x, y});
