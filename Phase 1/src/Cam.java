0609127b1fb5fa803cb40eeee2ea49c023b2ce06
alex07 <alexfrerichs@t-online.de>
2022-03-14 16:19:39 +0100

    ChaseCam class

---
+++ Phase 1/src/Cam.java
@@ -0,0 +1,44 @@
+import com.jme3.app.SimpleApplication;
+import com.jme3.asset.AssetManager;
+import com.jme3.input.ChaseCamera;
+import com.jme3.input.InputManager;
+import com.jme3.math.FastMath;
+import com.jme3.math.Vector3f;
+import com.jme3.renderer.Camera;
+import com.jme3.scene.Node;
+import com.jme3.scene.Spatial;
+
+public class Cam extends SimpleApplication{
+
+    private ChaseCamera chaseCam;
+    private Spatial player;
+    private Node rootNode;
+    private final InputManager inputManager;
+    private final AssetManager assetManager;
+    private final Camera cam;
+    private Vector3f flyDirection = Vector3f.ZERO;
+
+    public Cam (SimpleApplication app){
+        // player = Main.player;
+        rootNode = app.getRootNode();
+        inputManager = app.getInputManager();
+        assetManager = app.getAssetManager();
+        cam = app.getCamera();
+    }
+
+    @Override
+    public void simpleInitApp() {
+        flyCam.setEnabled(false);               // disable the default first-person camera
+        chaseCam = new ChaseCamera(cam, player, inputManager);      // 3rd person camera following the ball's motion
+        chaseCam.setSmoothMotion(true);
+        chaseCam.setDefaultDistance(40f);       // default distance to the target (ball)
+        chaseCam.setChasingSensitivity(5f);     // the lower the sensitivity the slower the camera will follow the target when it moves
+        chaseCam.setRotationSensitivity(5f);
+        chaseCam.setMaxDistance(50f);
+        chaseCam.setMinDistance(30f);
+        chaseCam.setDefaultVerticalRotation(-FastMath.PI/2);
+        chaseCam.setDefaultHorizontalRotation(-FastMath.PI/2);
+
+    }
+
+}
