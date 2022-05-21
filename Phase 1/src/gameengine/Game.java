cbe1d509afff9c85cca71b0f079d1f6299950f34
VitalijsTickovs <v.tickovs@student.maastrichtuniversity.nl>
2022-05-21 23:13:51 +0200

    JME3 is fucked up, it upscaled all the coordinates by 10.24, fixed all of that and now 3d is in sync with minimap. Added a bot support(most of the functions are borrowed from Game.java, so maybe later we could refactor some stuff)

--- Phase 1/src/gameengine/Game.java
+++ Phase 1/src/gameengine/Game.java
@@ -50,7 +50,8 @@ public class Game extends JPanel implements Runnable, GameObject {
         FPS = fps;
         createGameState();
         resetStartingVariables();
-        setManualInputType();
+        //setManualInputType();
+        setupInitialBot();
         // createTerrain();
         createCamera();
         createRenderer();
