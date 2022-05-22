8d85660d7ec684ad72cefcf28c85de5ccb2d3f93
Stan <stasostaszewski@gmail.com>
2022-05-22 11:41:08 +0200

    ah, yes, undo debugging

--- Phase 1/src/gameengine/Game.java
+++ Phase 1/src/gameengine/Game.java
@@ -301,8 +301,7 @@ public class Game extends JPanel implements Runnable, GameObject, MouseListener 
 
     // region Render
     public void render() {
-        // BufferedImage gameStateImage = gameStateRenderer.getSubimage(camera, drawArrow, true);
-        BufferedImage gameStateImage = gameStateRenderer.getMinimap(camera);
+        BufferedImage gameStateImage = gameStateRenderer.getSubimage(camera, drawArrow, true);
         drawImage(gameStateImage);
         gameStateImage.flush();
     }
