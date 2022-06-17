7e0c48ab3cc3f78e6801975574c355e71942610a
lode <91753873+tomasschreuder@users.noreply.github.com>
2022-06-17 23:01:27 +0200

    Update MenuGUI.java
    Cleaned up menu. Removed scrapped features.

--- Phase 1/src/gui/MenuGUI.java
+++ Phase 1/src/gui/MenuGUI.java
@@ -4,15 +4,11 @@ import visualization.gameengine.Game;
 import visualization.jmonkeyrender.Renderer;
 
 public class MenuGUI extends javax.swing.JFrame {
-    static boolean gameBoolean =true;//false =player game; true =bot game
-    static int levelNum;//0 =Earth; 1 =Moon; 2 =Mars; 3 =Test
     public static String texPath;
     
     public MenuGUI() {
         initComponents();
-        botRB.setSelected(gameBoolean);//bot game is default
-        playerRB.setSelected(!gameBoolean);
-
+        
         levelCB.getSelectedIndex();
         getTexPath();
     }
@@ -39,42 +35,24 @@ public class MenuGUI extends javax.swing.JFrame {
 
         buttonGroup1 = new javax.swing.ButtonGroup();
         jPanel3 = new javax.swing.JPanel();
-        levelCB = new javax.swing.JComboBox<>();
-        jLabel1 = new javax.swing.JLabel();
         singleB = new javax.swing.JButton();
         multiB = new javax.swing.JButton();
         exitB = new javax.swing.JButton();
-        botRB = new javax.swing.JRadioButton();
-        playerRB = new javax.swing.JRadioButton();
-        jLabel2 = new javax.swing.JLabel();
-        highscoreB = new javax.swing.JButton();
-        levelDetailsB = new javax.swing.JButton();
         jLabel3 = new javax.swing.JLabel();
+        levelCB = new javax.swing.JComboBox<>();
+        jLabel5 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Menu");
         setCursor(new java.awt.Cursor(java.awt.Cursor.CROSSHAIR_CURSOR));
 
         jPanel3.setBackground(new java.awt.Color(0, 102, 0));
-
-        levelCB.setBackground(new java.awt.Color(0, 153, 0));
-        levelCB.setFont(new java.awt.Font("Tw Cen MT Condensed Extra Bold", 0, 24)); // NOI18N
-        levelCB.setForeground(new java.awt.Color(51, 51, 51));
-        levelCB.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Earth", "Moon", "Mars", "Test" }));
-        levelCB.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                levelCBActionPerformed(evt);
-            }
-        });
-
-        jLabel1.setFont(new java.awt.Font("Rockwell Condensed", 0, 24)); // NOI18N
-        jLabel1.setForeground(new java.awt.Color(51, 51, 51));
-        jLabel1.setText("AGAINST");
+        jPanel3.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(255, 0, 255), 3));
 
         singleB.setBackground(new java.awt.Color(0, 204, 0));
         singleB.setFont(new java.awt.Font("Tw Cen MT Condensed Extra Bold", 0, 24)); // NOI18N
         singleB.setForeground(new java.awt.Color(51, 51, 51));
-        singleB.setText("2D");
+        singleB.setText("3D");
         singleB.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(51, 51, 255), 4));
         singleB.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -85,7 +63,7 @@ public class MenuGUI extends javax.swing.JFrame {
         multiB.setBackground(new java.awt.Color(0, 204, 0));
         multiB.setFont(new java.awt.Font("Tw Cen MT Condensed Extra Bold", 0, 24)); // NOI18N
         multiB.setForeground(new java.awt.Color(51, 51, 51));
-        multiB.setText("3D");
+        multiB.setText("2D");
         multiB.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(51, 51, 255), 4));
         multiB.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -104,120 +82,69 @@ public class MenuGUI extends javax.swing.JFrame {
             }
         });
 
-        buttonGroup1.add(botRB);
-        botRB.setFont(new java.awt.Font("Tw Cen MT Condensed Extra Bold", 0, 14)); // NOI18N
-        botRB.setText("BOT");
-        botRB.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                botRBActionPerformed(evt);
-            }
-        });
-
-        buttonGroup1.add(playerRB);
-        playerRB.setFont(new java.awt.Font("Tw Cen MT Condensed Extra Bold", 0, 14)); // NOI18N
-        playerRB.setText("PLAYER");
-        playerRB.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                playerRBActionPerformed(evt);
-            }
-        });
-
-        jLabel2.setFont(new java.awt.Font("Rockwell Condensed", 0, 24)); // NOI18N
-        jLabel2.setForeground(new java.awt.Color(51, 51, 51));
-        jLabel2.setText("LEVEL:");
-
-        highscoreB.setBackground(new java.awt.Color(0, 204, 0));
-        highscoreB.setFont(new java.awt.Font("Tw Cen MT Condensed Extra Bold", 0, 18)); // NOI18N
-        highscoreB.setForeground(new java.awt.Color(51, 51, 51));
-        highscoreB.setText("HIGHSCORES");
-        highscoreB.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(51, 51, 255), 4));
-        highscoreB.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                highscoreBActionPerformed(evt);
-            }
-        });
-
-        levelDetailsB.setBackground(new java.awt.Color(0, 204, 0));
-        levelDetailsB.setFont(new java.awt.Font("Tw Cen MT Condensed Extra Bold", 0, 18)); // NOI18N
-        levelDetailsB.setForeground(new java.awt.Color(51, 51, 51));
-        levelDetailsB.setText("LEVEL DETAILS");
-        levelDetailsB.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(51, 51, 255), 4));
-        levelDetailsB.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                levelDetailsBActionPerformed(evt);
-            }
-        });
-
-        jLabel3.setFont(new java.awt.Font("Rockwell Extra Bold", 1, 48)); // NOI18N
+        jLabel3.setBackground(new java.awt.Color(0, 102, 255));
+        jLabel3.setFont(new java.awt.Font("Rockwell Extra Bold", 1, 36)); // NOI18N
         jLabel3.setForeground(new java.awt.Color(255, 51, 153));
         jLabel3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel3.setText("CRAZY PUTTING");
+
+        levelCB.setBackground(new java.awt.Color(0, 204, 0));
+        levelCB.setFont(new java.awt.Font("Tw Cen MT Condensed Extra Bold", 0, 24)); // NOI18N
+        levelCB.setForeground(new java.awt.Color(51, 51, 51));
+        levelCB.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Hill Climbing", "Gradient Descent", "Rule Based" }));
+        levelCB.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                levelCBActionPerformed(evt);
+            }
+        });
+
+        jLabel5.setFont(new java.awt.Font("Rockwell Extra Bold", 1, 18)); // NOI18N
+        jLabel5.setForeground(new java.awt.Color(0, 0, 255));
+        jLabel5.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel5.setText("LEVEL");
+        jLabel5.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
 
         javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
         jPanel3.setLayout(jPanel3Layout);
         jPanel3Layout.setHorizontalGroup(
             jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel3Layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addContainerGap())
             .addGroup(jPanel3Layout.createSequentialGroup()
-                .addGap(77, 77, 77)
-                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel3Layout.createSequentialGroup()
-                        .addComponent(jLabel1)
-                        .addGap(82, 82, 82))
+                        .addContainerGap()
+                        .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, 432, Short.MAX_VALUE))
                     .addGroup(jPanel3Layout.createSequentialGroup()
-                        .addComponent(botRB)
-                        .addGap(18, 18, 18)
-                        .addComponent(playerRB)
-                        .addGap(54, 54, 54)))
-                .addComponent(jLabel2)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(levelCB, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(89, Short.MAX_VALUE))
+                        .addContainerGap()
+                        .addComponent(exitB, javax.swing.GroupLayout.PREFERRED_SIZE, 86, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 0, Short.MAX_VALUE)))
+                .addContainerGap())
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel3Layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGap(0, 0, Short.MAX_VALUE)
                 .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(multiB, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(singleB, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(142, 142, 142))
+                .addGap(120, 120, 120))
             .addGroup(jPanel3Layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(exitB, javax.swing.GroupLayout.PREFERRED_SIZE, 86, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(highscoreB, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(levelDetailsB, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap())
+                .addGap(125, 125, 125)
+                .addComponent(levelCB, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addComponent(jLabel5, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         jPanel3Layout.setVerticalGroup(
             jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel3Layout.createSequentialGroup()
-                .addGap(56, 56, 56)
-                .addComponent(jLabel3)
-                .addGap(69, 69, 69)
+                .addGap(36, 36, 36)
+                .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 97, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(singleB, javax.swing.GroupLayout.PREFERRED_SIZE, 48, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(multiB, javax.swing.GroupLayout.PREFERRED_SIZE, 48, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(49, 49, 49)
-                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel3Layout.createSequentialGroup()
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 14, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(jLabel2)
-                            .addComponent(levelCB, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                    .addGroup(jPanel3Layout.createSequentialGroup()
-                        .addComponent(jLabel1)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(playerRB)
-                            .addComponent(botRB))))
-                .addGap(60, 60, 60)
-                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(levelDetailsB, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(highscoreB, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(exitB, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(33, 33, 33)
+                .addComponent(jLabel5)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(levelCB, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 64, Short.MAX_VALUE)
+                .addComponent(exitB, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
         );
 
@@ -259,27 +186,6 @@ public class MenuGUI extends javax.swing.JFrame {
     private void exitBActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_exitBActionPerformed
         System.exit(0);
     }//GEN-LAST:event_exitBActionPerformed
-
-    private void highscoreBActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_highscoreBActionPerformed
-        new HighscoreGUI().setVisible(true);
-        this.setVisible(false);
-    }//GEN-LAST:event_highscoreBActionPerformed
-
-    private void levelDetailsBActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_levelDetailsBActionPerformed
-        
-    }//GEN-LAST:event_levelDetailsBActionPerformed
-
-    private void botRBActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botRBActionPerformed
-        gameBoolean =true;
-    }//GEN-LAST:event_botRBActionPerformed
-
-    private void playerRBActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_playerRBActionPerformed
-        gameBoolean =false;
-    }//GEN-LAST:event_playerRBActionPerformed
-
-    private void levelCBActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_levelCBActionPerformed
-        levelNum = levelCB.getSelectedIndex(); 
-    }//GEN-LAST:event_levelCBActionPerformed
     
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
@@ -313,19 +219,14 @@ public class MenuGUI extends javax.swing.JFrame {
         });
     }
 
-    // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JRadioButton botRB;
-    private javax.swing.ButtonGroup buttonGroup1;
-    private javax.swing.JButton exitB;
-    private javax.swing.JButton highscoreB;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JPanel jPanel3;
-    private static javax.swing.JComboBox<String> levelCB;
-    private javax.swing.JButton levelDetailsB;
-    private javax.swing.JButton multiB;
-    private javax.swing.JRadioButton playerRB;
-    private javax.swing.JButton singleB;
-    // End of variables declaration//GEN-END:variables
+   // Variables declaration - do not modify//GEN-BEGIN:variables
+   private javax.swing.ButtonGroup buttonGroup1;
+   private javax.swing.JButton exitB;
+   private javax.swing.JLabel jLabel3;
+   private javax.swing.JLabel jLabel5;
+   private javax.swing.JPanel jPanel3;
+   private static javax.swing.JComboBox<String> levelCB;
+   private javax.swing.JButton multiB;
+   private javax.swing.JButton singleB;
+   // End of variables declaration//GEN-END:variables
 }
